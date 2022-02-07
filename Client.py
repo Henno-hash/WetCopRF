8ec302326b8c179ba2d24c3f12088cb1f2722b2d
Henno-hash <80264956+Henno-hash@users.noreply.github.com>
2022-02-07 17:14:22 +0100

    Update Client.py
    import fix

--- Client.py
+++ Client.py
@@ -1,4 +1,4 @@
-from wetCopRF.py import *
+import utils
 if __name__ == "__main__":
     # Client
     # Please set your workspace location (Requires at least twice the free space of the input data)
