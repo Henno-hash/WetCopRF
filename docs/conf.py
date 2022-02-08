3ce62c8275e58778b5570734cd04628a2bd55b33
Henno-hash <80264956+Henno-hash@users.noreply.github.com>
2022-02-08 13:42:18 +0100

    Update conf.py
    path2

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,10 @@
 
 import os
 import sys
-sys.path.insert(0,os.path.abspath('..'))
-sys.path.insert(0,os.path.abspath('../utils'))
+sys.path.insert('/utils')
+sys.path.insert('')
+#sys.path.insert(0,os.path.abspath('..'))
+#sys.path.insert(0,os.path.abspath('../utils'))
 
 # -- Project information -----------------------------------------------------
 
