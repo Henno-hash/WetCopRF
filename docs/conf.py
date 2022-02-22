102fc7d504b67a5998ba0d7aa0f206fbe6a0d473
Henno-hash <80264956+Henno-hash@users.noreply.github.com>
2022-02-22 12:33:41 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.append(os.path.abspath('..'))
-sys.path.append(os.path.abspath('..\wetCopRF'))
+#import os
+#import sys
+#sys.path.append(os.path.abspath('..'))
+#sys.path.append(os.path.abspath('..\wetCopRF'))
 
 # -- Project information -----------------------------------------------------
 
